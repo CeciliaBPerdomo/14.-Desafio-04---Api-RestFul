1ccd71e8c4c77fd3909675828ae1346d7c2a3497
Cecilia <cecilia.perdomo@gmail.com>
2022-03-29 20:48:47 -0300

    casifinal

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+// Consigna: Realizar un proyecto de servidor basado en node.js y express que 
+// ofrezca una API RESTful de productos. En detalle, que incorpore las siguientes 
+//rutas:
+
 import express from 'express'
 
 const app = express()
@@ -8,23 +12,56 @@ routerProductos.use(express.json())
 routerProductos.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 
-const productos = []
-/* ---- Guardar ----- */
+const productos = [
+    {
+        nombre: 'Escuadra',
+        precio: 25,
+        thumbnail: 'www.fotoescuadra.com'
+    },
+    {
+        nombre: 'Regla',
+        precio: 20,
+        thumbnail: 'www.fotoregla.com'
+    } 
+
+]
+
+// recibe y agrega un producto, y lo devuelve con su id asignado.
 routerProductos.post('/guardar', (req, res) => {
     if (req.body !== "") {
         let idProd = productos.length + 1
-        const newProd = req.body + {id: idProd}
-        productos.push(newProd)
-        res.json(newProd)
-        //res.send('Producto guardado con exito')
+        productos.push(req.body)
+        res.json(req.body)
     } else {
         res.json('Los campos están vacios')
     }
 })
 
-/* ----- Listar ------------ */
+// Devuelve todos los productos.
 routerProductos.get('/listar', (req, res) => {
     res.json(productos)
+})
+
+// Devuelve un producto según su id.
+routerProductos.get('/listar/:id', (req, res) => {
+    const { id } = req.params
+    res.send({ producto: productos[parseInt(id) - 1]})
+})
+
+// recibe y actualiza un producto según su id.
+routerProductos.put('/modificar/:id', (req, res) => {
+    const { prod } = req.body
+    const { id } = req.params
+    const anterior = productos[parseInt(id) - 1]
+    productos[parseInt(id) - 1] = prod
+    res.json({Actualizada: productos[parseInt(id) - 1], Anterior: anterior})
+})
+
+// elimina un producto según su id.
+app.delete('/borrar/:id', (req, res) => {
+    const { id } = req.params
+    const prod = productos[parseInt(id) - 1]
+    res.send({ borrado: prod })
 })
 
 /* ---- Server Listen ----- */
